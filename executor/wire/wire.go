0e2b5cd98f081a779b0b1188f1d0a48ee61bf981
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-29 22:00:06 +0100

    Send ack when command exits

--- executor/wire/wire.go
+++ executor/wire/wire.go
@@ -6,3 +6,9 @@ type RunMessage struct {
 	// Command is a command to execute
 	Command string
 }
+
+// Ack is sent when command finishes
+type Ack struct {
+	// Error is the error returned by command
+	Error string
+}
