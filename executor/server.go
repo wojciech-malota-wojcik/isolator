7b785a918010fccf7323993ff5fd5a00151b9728
Wojciech Małota-Wójcik <59281144+wojciech-malota-wojcik@users.noreply.github.com>
2021-12-01 19:48:54 +0100

    Transmit logs over wire instead of printing them (#12)

--- executor/server.go
+++ executor/server.go
@@ -11,15 +11,53 @@ import (
 	"sync"
 	"syscall"
 
-	"github.com/wojciech-malota-wojcik/isolator/client"
-
 	"github.com/ridge/parallel"
-	"github.com/wojciech-malota-wojcik/isolator/executor/wire"
+	"github.com/wojciech-malota-wojcik/isolator/client"
+	"github.com/wojciech-malota-wojcik/isolator/client/wire"
 	"github.com/wojciech-malota-wojcik/libexec"
+	"github.com/wojciech-malota-wojcik/logger"
+	"go.uber.org/zap"
 )
+
+type logTransmitter struct {
+	client *client.Client
+	stream wire.Stream
+
+	buf []byte
+}
+
+func (lt *logTransmitter) Write(data []byte) (int, error) {
+	length := len(lt.buf) + len(data)
+	if length < 100 {
+		lt.buf = append(lt.buf, data...)
+		return len(data), nil
+	}
+	buf := make([]byte, length)
+	copy(buf, lt.buf)
+	copy(buf[len(lt.buf):], data)
+	err := lt.client.Send(wire.Log{Stream: lt.stream, Text: string(buf)})
+	if err != nil {
+		return 0, err
+	}
+	lt.buf = make([]byte, 0, len(lt.buf))
+	return len(data), nil
+}
+
+func (lt *logTransmitter) Flush() error {
+	if len(lt.buf) == 0 {
+		return nil
+	}
+	if err := lt.client.Send(wire.Log{Stream: lt.stream, Text: string(lt.buf)}); err != nil {
+		return err
+	}
+	lt.buf = make([]byte, 0, len(lt.buf))
+	return nil
+}
 
 // Run runs isolator server
 func Run(ctx context.Context) (retErr error) {
+	log := logger.Get(ctx)
+
 	if err := os.MkdirAll("/proc", 0o755); err != nil && !os.IsExist(err) {
 		return err
 	}
@@ -27,8 +65,11 @@ func Run(ctx context.Context) (retErr error) {
 		return fmt.Errorf("mounting proc failed: %w", err)
 	}
 	defer func() {
-		if err := syscall.Unmount("/proc", 0); retErr == nil {
-			retErr = err
+		if err := syscall.Unmount("/proc", 0); err != nil {
+			log.Error("Unmounting /proc failed", zap.Error(err))
+			if retErr == nil {
+				retErr = fmt.Errorf("unmounting /proc failed: %w", err)
+			}
 		}
 	}()
 
@@ -37,8 +78,12 @@ func Run(ctx context.Context) (retErr error) {
 		return fmt.Errorf("failed to start listening: %w", err)
 	}
 	defer func() {
-		_ = l.Close()
-		_ = os.Remove(wire.SocketPath)
+		if err := l.Close(); err != nil {
+			log.Error("Closing listening socket failed", zap.Error(err))
+		}
+		if err := os.Remove(wire.SocketPath); err != nil && !os.IsNotExist(err) {
+			log.Error("Removing unix socket file failed", zap.Error(err))
+		}
 	}()
 
 	var mu sync.Mutex
@@ -75,11 +120,29 @@ func Run(ctx context.Context) (retErr error) {
 					return errors.New("unexpected message received")
 				}
 
-				var errStr string
+				outTransmitter := &logTransmitter{
+					stream: wire.StreamOut,
+					client: c,
+				}
+				errTransmitter := &logTransmitter{
+					stream: wire.StreamErr,
+					client: c,
+				}
+
 				cmd := exec.Command("/bin/sh", "-c", execute.Command)
+				cmd.Stdout = outTransmitter
+				cmd.Stderr = errTransmitter
+				var errStr string
 				if err := libexec.Exec(ctx, cmd); err != nil {
 					errStr = err.Error()
 				}
+				if err := outTransmitter.Flush(); err != nil {
+					log.Error("Flushing stdout log failed", zap.Error(err))
+				}
+				if err := errTransmitter.Flush(); err != nil {
+					log.Error("Flushing stderr log failed", zap.Error(err))
+				}
+
 				if err := c.Send(wire.Completed{
 					ExitCode: cmd.ProcessState.ExitCode(),
 					Error:    errStr,
