a6af2f490f690be676f0998745c584ecefe2eb94
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-05-07 19:03:41 +0200

    Upgrade `build`

--- build/go.mod
+++ build/go.mod
@@ -3,8 +3,6 @@ module build
 go 1.16
 
 require (
-	github.com/outofforest/build v1.7.13
-	github.com/outofforest/buildgo v0.4.2
-	github.com/outofforest/ioc/v2 v2.5.0
-	github.com/outofforest/run v0.2.3
+	github.com/outofforest/build v1.8.0
+	github.com/outofforest/buildgo v0.4.3
 )
--- build/index.go
+++ build/index.go
@@ -1,11 +1,14 @@
 package build
 
-import "github.com/outofforest/buildgo"
+import (
+	"github.com/outofforest/build"
+	"github.com/outofforest/buildgo"
+)
 
 // Commands is a definition of commands available in build system
-var Commands = map[string]interface{}{
-	"setup": setup,
-	"build": buildApp,
+var Commands = map[string]build.Command{
+	"setup": {Fn: setup, Description: "Installs tools required by development environment"},
+	"build": {Fn: buildApp, Description: "Builds isolator binary and adds it to the code"},
 }
 
 func init() {
