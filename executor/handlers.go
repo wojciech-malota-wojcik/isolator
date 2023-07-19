977e4b95dd8bcc63ede3a020a87f296c4debfcc7
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-07-19 17:50:29 +0200

    Clean up logging (#144)

--- executor/handlers.go
+++ executor/handlers.go
@@ -1,17 +1,13 @@
 package executor
 
 import (
-	"bytes"
 	"context"
 	"os/exec"
-	"sync"
-	"time"
 
 	"github.com/outofforest/libexec"
 	"github.com/outofforest/logger"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 
 	"github.com/outofforest/isolator/lib/docker"
 	"github.com/outofforest/isolator/lib/libhttp"
@@ -82,7 +78,7 @@ func NewRunEmbeddedFunctionHandler(funcs map[string]EmbeddedFunc) HandlerFunc {
 		log := logger.Get(ctx)
 		log.Info("Starting embedded function")
 
-		if err := fn(zapTransmitter(ctx, encode), m.Args); err != nil {
+		if err := fn(withZAPTransmitter(ctx, encode), m.Args); err != nil {
 			log.Error("Embedded function exited with error", zap.Error(err))
 			return err
 		}
@@ -116,91 +112,4 @@ func ExecuteHandler(ctx context.Context, content interface{}, encode wire.Encode
 
 	log.Info("Command exited")
 	return nil
-}
-
-func newLogTransmitter(encode wire.EncoderFunc) *logTransmitter {
-	return &logTransmitter{
-		encode: encode,
-	}
-}
-
-type logTransmitter struct {
-	encode wire.EncoderFunc
-
-	mu     sync.Mutex
-	buf    []byte
-	start  int
-	end    int
-	length int
-}
-
-func (lt *logTransmitter) Write(data []byte) (int, error) {
-	lt.mu.Lock()
-	defer lt.mu.Unlock()
-
-	bufLen := 1024
-
-	dataLength := len(data)
-	if dataLength == 0 {
-		return 0, nil
-	}
-
-	if len(lt.buf[lt.end:]) < dataLength {
-		if newBufLen := lt.length + dataLength; bufLen < newBufLen {
-			bufLen = newBufLen
-		}
-		newBuf := make([]byte, bufLen)
-		if lt.length > 0 {
-			copy(newBuf, lt.buf[lt.start:lt.end])
-		}
-		lt.buf = newBuf
-		lt.start = 0
-		lt.end = lt.length
-	}
-	copy(lt.buf[lt.end:], data)
-	lt.end += dataLength
-	lt.length += dataLength
-
-	for {
-		if lt.length == 0 {
-			break
-		}
-		pos := bytes.IndexByte(lt.buf[lt.start:lt.end], '\n')
-		if pos < 0 {
-			break
-		}
-
-		if pos > 0 {
-			err := lt.encode(wire.Log{Time: time.Now().UTC(), Content: lt.buf[lt.start : lt.start+pos]})
-			if err != nil {
-				return 0, err
-			}
-		}
-
-		lt.start += pos + 1
-		lt.length -= pos + 1
-
-		if lt.start == lt.end {
-			lt.start = 0
-			lt.end = 0
-		}
-	}
-
-	return dataLength, nil
-}
-
-func (lt *logTransmitter) Sync() error {
-	return nil
-}
-
-func zapTransmitter(ctx context.Context, encode wire.EncoderFunc) context.Context {
-	transmitter := newLogTransmitter(encode)
-	transmitCore := zapcore.NewCore(zapcore.NewJSONEncoder(logger.EncoderConfig), transmitter, zap.NewAtomicLevelAt(zap.DebugLevel))
-
-	log := logger.Get(ctx)
-	log = log.WithOptions(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
-		return zapcore.NewTee(core, transmitCore)
-	}))
-
-	return logger.WithLogger(ctx, log)
 }
